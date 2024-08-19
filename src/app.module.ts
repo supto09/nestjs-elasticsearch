5096a406e2a43aa1e498722a23b95f25af860465
Ashik Saeed <ashik@cloudly.io>
2024-08-19 17:46:54 +0600

    health check endpoint added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { ConfigModule } from '@/config/config.module';
 import { SearchModule } from '@/search/search.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule, SearchModule, UsersModule, DatabaseModule],
+  imports: [
+    ConfigModule,
+    SearchModule,
+    UsersModule,
+    DatabaseModule,
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
